e9666960fd11160822951f39f4c4920638754308
solarpark7346 <solarpark7346@gmail.com>
2023-02-26 01:43:05 +0900

    mysql 연동

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+
 public class DemoApplication {
 
 	@RequestMapping("/")
