f447cb902c1a8bf1ad74e7e0d2dfde6e601407ec
solarpark7346 <solarpark7346@gmail.com>
2023-03-05 15:49:23 +0900

    데이터 타입 오류 수정 및 JPA

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,10 +1,18 @@
 package hello.hellospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String name;
-    private String nickName;
 
     public long getId() {
         return id;
@@ -20,9 +28,5 @@ public class Member {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public void setNickName(String nickName){
-        this.nickName = nickName;
     }
 }
